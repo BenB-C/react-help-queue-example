29605e9b18b47367937005ce14f4012a17c42ac1
BenB-C <bcauchon@gmail.com>
2019-09-16 16:30:32 -0700

    Importing Reducer Files

--- __tests__/ticket-list-reducer.test.js
+++ __tests__/ticket-list-reducer.test.js
@@ -1,7 +1,9 @@
+import ticketListReducer from './../src/reducers/ticket-list-reducer';
+
 describe('ticketListReducer', () => {
 
-  test('Temporary dummy test: two plus two is four', () => {
-    expect(2 + 2).toBe(4);
+  test('Should return default state if no action type is recognized', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
 });
